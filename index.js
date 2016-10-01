c3abe6d77174b42964d566c998799518503f45e9
kendradalley <kendracdalley@gmail.com>
2016-10-01 15:51:28 -0700

    listener and export module

--- index.js
+++ index.js
@@ -52,8 +52,11 @@ app.post('/api/auth', function(req, res){
 // Angular catchall
 app.get('/*', function(req, res){
   res.sendFile(path.join(__dirname, 'public/index.html'));
-
 });
+
+var server = app.listen(process.env.PORT || 3000);
+
+module.exports = server;
 
 
 
